1328e7da3024fe001fcd2546cbee2000fbcb6f89
rui.zhang <rui.zhang@jiduauto.com>
2024-03-13 18:09:37 +0800

    1. 设置首页

---
+++ src/view/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<el-container class="home_container">
+		<nav-bar is-collapse="false"></nav-bar>
+
+	</el-container>
+</template>
+
+<script setup>
+</script>
+
+<style lang="less" scoped>
+
+.home_container {
+	height: 100%;
+}
+
+</style>
