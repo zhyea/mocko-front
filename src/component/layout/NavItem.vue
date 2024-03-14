fddaa37db04723f45ee4fac84de7898d8ec04340
zhyea <zhangrui@zhyea.com>
2024-03-15 00:06:13 +0800

    1. 完成nav-item
    Change-Id: I20037fda3b6c94c5ca3e7e65b99bf4c49ced8ddd

--- src/component/layout/NavItem.vue
+++ src/component/layout/NavItem.vue
@@ -2,14 +2,36 @@
 	<!-- parent -->
 	<el-sub-menu
 		:index="props.item.uri"
-		v-if="props.item.children"
-	>
+		v-if="props.item.children">
 
+		<template #title>
+			<el-icon v-if="props.item.icon">
+				<component :is="icon(props.item.icon)"/>
+			</el-icon>
+			<span>{{ props.item.name }}</span>
+		</template>
+
+		<template v-for="child in props.item.children">
+			<nav-item v-if="child.children" :item="child" :key="child"/>
+			<el-menu-item v-else :index="child.uri" :key="child.uri">
+				<el-icon v-if="child.icon">
+					<component :is="icon(child.icon)"/>
+				</el-icon>
+				{{ child.name }}
+			</el-menu-item>
+		</template>
 	</el-sub-menu>
+
 
 	<!-- children -->
-	<el-sub-menu>
-	</el-sub-menu>
+	<el-menu-item
+		:index="props.item.uri"
+		v-else>
+		<el-icon v-if="props.item.icon">
+			<component :is="icon(props.item.icon)"/>
+		</el-icon>
+		<span>{{ props.item.name }}</span>
+	</el-menu-item>
 </template>
 
 <script setup>
