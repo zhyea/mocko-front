9ad7a798f9e552f611996a3cfd9e584fcfd1f352
zhyea <zhangrui@zhyea.com>
2024-03-14 08:57:57 +0800

    1. 开始sidebar组件
    Change-Id: I6f5a95a97fb3fdd734659346a63d523a948d10b3

--- src/component/layout/Sidebar.vue
+++ src/component/layout/Sidebar.vue
@@ -1,5 +1,9 @@
 <template>
 	<el-aside :width="collapsed ? '65px' : '250px'">
+		<div class="sidebar-box">
+			<div class="logo-box">
+			</div>
+		</div>
 	</el-aside>
 </template>
 
@@ -11,5 +15,28 @@ const props = defineProps({
 
 
 <style scoped lang="less">
+.sidebar-box {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	background: #263238;
+	border-right: 1px solid #e6e6e6;
+}
 
+.logo {
+	height: 60px;
+	display: flex;
+	line-height: 60px;
+	color: #42b983;
+	width: 100%;
+
+	.normal {
+		padding-left: 20px;
+	}
+
+	.mini {
+		width: 100%;
+		text-align: center;
+	}
+}
 </style>
